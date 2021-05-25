9000834a332e4c1c65fcca6285af33981292c6a7
unknown <jessbuenos@gmail.com>
2021-05-25 15:18:22 +0200

    idea folder removed

--- page_jumper.py
+++ page_jumper.py
@@ -9,13 +9,18 @@ from selenium.webdriver.common.keys import Keys
 class PageJumper:
 
     def __init__(self, url):
-        #driver = webdriver.Chrome()
-        driver = webdriver.Firefox()
-        driver.implicitly_wait(30)
-        driver.get(url)
+        # driver = webdriver.Chrome()
+        self.driver: webdriver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.driver.get(url)
+        self.soup: BeautifulSoup = BeautifulSoup(driver.page_source)
+        self.links: List = []
 
+    def jump_page(self):
+        pass
+
+    def extract_links(self):
         # And then it's like Beautiful soup
-        soup = BeautifulSoup(driver.page_source)
         print(soup)
         for elem in soup.find_all('a', attrs={"class": "card__title-link"}):
             print('The link ("href") is: ', elem.get('href'))
@@ -23,4 +28,4 @@ class PageJumper:
 
 
 pageJumper = PageJumper('https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=relevance')
-#print(pageJumper.soup)
+# print(pageJumper.soup)
