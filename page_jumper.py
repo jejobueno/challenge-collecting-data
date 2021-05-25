a6ab81dfbca81e9a1969bdf70d513e30d19e9d68
unknown <jessbuenos@gmail.com>
2021-05-25 14:18:49 +0200

    firefox driver

--- page_jumper.py
+++ page_jumper.py
@@ -9,13 +9,17 @@ from selenium.webdriver.common.keys import Keys
 class PageJumper:
 
     def __init__(self, url):
-        driver = webdriver.Chrome()
+        #driver = webdriver.Chrome()
+        driver = webdriver.Firefox()
         driver.implicitly_wait(30)
         driver.get(url)
 
         # And then it's like Beautiful soup
         soup = BeautifulSoup(driver.page_source)
         print(soup)
+        for elem in soup.find_all('a', attrs={"class": "card__title-link"}):
+            print('The link ("href") is: ', elem.get('href'))
+            print('House: ', elem.get('aria-label'), "\n")
 
 
 pageJumper = PageJumper('https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=relevance')
