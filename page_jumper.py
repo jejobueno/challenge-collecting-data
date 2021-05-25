8e6b9e8fdf62b2645a5990807443c2f12ece8379
unknown <jessbuenos@gmail.com>
2021-05-25 13:53:49 +0200

    selenium soup implementation

--- page_jumper.py
+++ page_jumper.py
@@ -1,18 +1,22 @@
+from time import strftime
+
 from bs4 import BeautifulSoup
 import requests
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
 
 class PageJumper:
 
     def __init__(self, url):
-        r = requests.get(url)
-        print(url, r.status_code)
-        self.soup = BeautifulSoup(r.content, 'lxml')
+        driver = webdriver.Chrome()
+        driver.implicitly_wait(30)
+        driver.get(url)
 
-        for elem in self.soup.find_all('a'):
-            print('The tag element is: ', elem)
-
-        print('loop ended')
+        # And then it's like Beautiful soup
+        soup = BeautifulSoup(driver.page_source)
+        print(soup)
 
 
 pageJumper = PageJumper('https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=relevance')
-print(pageJumper.soup)
+#print(pageJumper.soup)
