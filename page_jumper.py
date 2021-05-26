a24e33c66f30229078240a18f34596795a6853e9
unknown <jessbuenos@gmail.com>
2021-05-26 09:52:24 +0200

    page jumper function

--- page_jumper.py
+++ page_jumper.py
@@ -1,6 +1,7 @@
 from time import strftime
 
 from bs4 import BeautifulSoup
+import pandas as pd
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -8,24 +9,51 @@ from selenium.webdriver.common.keys import Keys
 
 class PageJumper:
 
-    def __init__(self, url):
-        # driver = webdriver.Chrome()
-        self.driver: webdriver = webdriver.Firefox()
-        self.driver.implicitly_wait(30)
-        self.driver.get(url)
-        self.soup: BeautifulSoup = BeautifulSoup(driver.page_source)
-        self.links: List = []
+    def jump_page(self, url: str):
+        if url.find('page=') > 0:
+            page_number = int(url[url.find('page=') + 5:url.find('&order')])
+            nextlink = url[0:url.find('page=') + 5] + str(page_number + 1) + url[url.find('&order'):]
+        else:
+            nextlink = url[0:url.find('BE')+3] + 'page=2' + url[url.find('&order'):]
+        return nextlink
 
-    def jump_page(self):
-        pass
+    def extract_links(self, url):
+        driver = webdriver.Chrome()
+        # driver = webdriver.Firefox()
+        driver.implicitly_wait(30)
+        driver.get(url)
+        soup: BeautifulSoup = BeautifulSoup(driver.page_source, 'lxml')
+        links: List = list()
 
-    def extract_links(self):
         # And then it's like Beautiful soup
-        print(soup)
         for elem in soup.find_all('a', attrs={"class": "card__title-link"}):
-            print('The link ("href") is: ', elem.get('href'))
-            print('House: ', elem.get('aria-label'), "\n")
+            links.append(elem.get('href'))
+        
+        driver.close()
+        
+        return links
 
 
-pageJumper = PageJumper('https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=relevance')
-# print(pageJumper.soup)
+    def scrap_pages(self, url: str):
+        links = list()
+        while True:
+
+            print(links)
+            page_links = self.extract_links(url)
+            if page_links or len(links)<10000:
+                links += page_links
+                url = self.jump_page(url)
+            else:
+                break
+        return links
+
+    def create_csv(self, url):
+        links = self.scrap_pages(url)
+        file_links_housing = pd.DataFrame(links)
+        file_links_housing.to_csv(".assets/links-inmoweb.csv", index=False)
+
+
+
+
+pageJumper = PageJumper()
+links = pageJumper.create_csv('https://www.immoweb.be/en/search/house-and-apartment/for-sale?countries=BE&orderBy=relevance')
